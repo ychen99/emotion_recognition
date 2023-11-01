96a429a2831e8bda9347de7e850e67356fb6c757
y_Chen <ashesthcy@gmail.com>
2023-11-01 17:00:58 +0100

    1 commit

--- pre_processing.py
+++ pre_processing.py
@@ -1,19 +1,60 @@
 import os
 import numpy as np
+import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 mpl.use('TkAgg')
 
+file_start = r"X:\BP4D+_v0.2\Physiology"
+dir_list = os.listdir(file_start)
+file_end = 'Pulse Rate_BPM.txt'
 
-file_path = r'X:\BP4D+_v0.2\Physiology\F001\T1\Pulse Rate_BPM.txt'
+''''
+label = pd.DataFrame({'label': [1] * 100})
 
-with open(file_path) as f:
-    data = f.read()
-    data_into_list = data.split("\n")
+for x in dir_list:
+    new_path = os.path.join(path, x, file_end)
+    if not os.path.exists(new_path):
+        print(f'{new_path} is not exist')
+        continue
+    try:
+        df_data = pd.read_csv(new_path, header=None)
+    except FileNotFoundError:
+        print(f'Error reading {new_path}')
+        continue
+    df_data.columns = [x]
+    new_data = pd.concat([label, df_data], axis=1)
+    label = new_data
+'''
 
 
-print(len(data_into_list))
-plt.figure(figsize=(16, 10))
-plt.plot(data_into_list)
-plt.show()
+def create_df_data(path):
+    columns = [f'{x}' for x in dir_list]
+    #columns = ['label'] + columns
+    new_data = pd.DataFrame(columns=columns)
+    for i in range(1, 10):
+        bridge = 'T' + f'{i}'
+        label = pd.DataFrame({'label': [i] * 100})
+        for x in dir_list:
+            new_path = os.path.join(path, x, bridge, file_end)
+            if not os.path.exists(new_path):
+                print(f'{new_path} is not exist')
+                continue
+            try:
+                df_data = pd.read_csv(new_path, header=None)
+            except FileNotFoundError:
+                print(f'Error reading {new_path}')
+                continue
+            n = df_data.shape[0]
+
+            new_data_ = pd.concat([label, df_data], axis=1)
+            label = new_data_
+        print(new_data_.shape, new_data.shape)
+        new_data = pd.concat([new_data.reset_index(drop=True),new_data_.reset_index(drop=True)],ignore_index=True)
+
+    # new_data.columns = ['label'] + columns
+    return new_data
+
+data = create_df_data(file_start)
+print(data.info)
