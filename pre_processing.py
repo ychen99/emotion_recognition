a709dde29fccd0e459aca98525dcaea62680c4f3
y_Chen <ashesthcy@gmail.com>
2023-11-03 11:08:09 +0100

    1 commit

--- pre_processing.py
+++ pre_processing.py
@@ -8,53 +8,36 @@ mpl.use('TkAgg')
 
 file_start = r"X:\BP4D+_v0.2\Physiology"
 dir_list = os.listdir(file_start)
-file_end = 'Pulse Rate_BPM.txt'
-
-''''
-label = pd.DataFrame({'label': [1] * 100})
-
-for x in dir_list:
-    new_path = os.path.join(path, x, file_end)
-    if not os.path.exists(new_path):
-        print(f'{new_path} is not exist')
-        continue
-    try:
-        df_data = pd.read_csv(new_path, header=None)
-    except FileNotFoundError:
-        print(f'Error reading {new_path}')
-        continue
-    df_data.columns = [x]
-    new_data = pd.concat([label, df_data], axis=1)
-    label = new_data
-'''
+file_end = 'Resp_Volts'+'.txt'
 
 
 def create_df_data(path):
     columns = [f'{x}' for x in dir_list]
-    #columns = ['label'] + columns
-    new_data = pd.DataFrame(columns=columns)
-    for i in range(1, 10):
+    new_data = pd.DataFrame()
+
+    for i in range(1, 11):
         bridge = 'T' + f'{i}'
-        label = pd.DataFrame({'label': [i] * 100})
+        dfs = pd.DataFrame()
+
         for x in dir_list:
             new_path = os.path.join(path, x, bridge, file_end)
-            if not os.path.exists(new_path):
+            if os.path.exists(new_path):
+                df_data = pd.read_csv(new_path, header=None)
+                df_combined = pd.concat([dfs, df_data], axis=1)
+                dfs = df_combined
+            else:
+                dfs[x] = [np.nan for _ in range(dfs.shape[0])]
                 print(f'{new_path} is not exist')
                 continue
-            try:
-                df_data = pd.read_csv(new_path, header=None)
-            except FileNotFoundError:
-                print(f'Error reading {new_path}')
-                continue
-            n = df_data.shape[0]
-
-            new_data_ = pd.concat([label, df_data], axis=1)
-            label = new_data_
-        print(new_data_.shape, new_data.shape)
-        new_data = pd.concat([new_data.reset_index(drop=True),new_data_.reset_index(drop=True)],ignore_index=True)
-
-    # new_data.columns = ['label'] + columns
+        dfs.columns = columns
+        label = pd.DataFrame({'label': [i] * dfs.shape[0]})
+        bridge_data = pd.concat([label, dfs], axis=1)
+        new_data = pd.concat([new_data, bridge_data], axis=0)
     return new_data
 
+
 data = create_df_data(file_start)
+# data.to_csv('X:\BP4D+_v0.2\Physiology\Pulse_Rate_BPM.csv', index=False,  encoding='utf-8')
 print(data.info)
+
+name = ['BP Dia_mmHg', 'BP_mmHg', 'EDA_microsiemens','LA Mean BP_mmHg','LA Systolic BP_mmHg','Pulse Rate_BPM','Resp_Volts','Respiration Rate_BPM']
