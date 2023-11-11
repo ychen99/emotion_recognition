e14157da51923e387cafdac3164110273f22f4f4
y_Chen <ashesthcy@gmail.com>
2023-11-11 12:25:59 +0100

    1 commit

--- pre_processing.py
+++ pre_processing.py
@@ -2,32 +2,35 @@ import os
 import numpy as np
 import pandas as pd
 import matplotlib as mpl
+import tsfel
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from zipfile import ZipFile
 
 mpl.use('TkAgg')
 
-name = ['BP Dia_mmHg.txt', 'BP_mmHg.txt', 'EDA_microsiemens.txt', 'LA Mean BP_mmHg.txt', 'LA Systolic BP_mmHg.txt',
-        'Pulse Rate_BPM.txt', 'Resp_Volts.txt', 'Respiration Rate_BPM.txt']
-
-file_start = r"X:\BP4D+_v0.2\Physiology"
-dir_list = os.listdir(file_start)
-
-column = ['BP Dia_mmHg', 'BP_mmHg', 'EDA_microsiemens', 'LA Mean BP_mmHg', 'LA Systolic BP_mmHg','Pulse Rate_BPM', 'Resp_Volts', 'Respiration Rate_BPM']
-
 
 def create_df_data(start, dir_list):
+    name = ['BP Dia_mmHg.txt', 'BP_mmHg.txt', 'EDA_microsiemens.txt', 'LA Mean BP_mmHg.txt', 'LA Systolic BP_mmHg.txt',
+            'Pulse Rate_BPM.txt', 'Resp_Volts.txt', 'Respiration Rate_BPM.txt']
+
+    file_start = r"X:\BP4D+_v0.2\Physiology"
+    dir_list = os.listdir(file_start)
+
+    column = ['BP Dia_mmHg', 'BP_mmHg', 'EDA_microsiemens', 'LA Mean BP_mmHg', 'LA Systolic BP_mmHg', 'Pulse Rate_BPM',
+              'Resp_Volts', 'Respiration Rate_BPM']
     all_data_frames = []
 
     for dir_ in dir_list:
         for i in range(1, 11):
             bridge = 'T' + f'{i}'
             bridge_data_frames = []
-            for name_ in name:
+            for name_, col_name in zip(name, column):
                 new_path = os.path.join(start, dir_, bridge, name_)
 
                 if os.path.exists(new_path):
                     df_data = pd.read_csv(new_path, header=None)
+                    df_data.columns = [col_name]
                     bridge_data_frames.append(df_data)
                 else:
                     print(f'{new_path} is not exist')
@@ -35,17 +38,104 @@ def create_df_data(start, dir_list):
                 # Merge all data from the same bridge
                 bridge_df = pd.concat(bridge_data_frames, axis=1)
 
-                bridge_df['label'] = i
-                bridge_df['id'] = dir_
+                bridge_df.insert(0, 'id', dir_)
+                bridge_df.insert(1, 'label', i)
                 all_data_frames.append(bridge_df)
 
     # Merge all data frames at once
     result_data = pd.concat(all_data_frames, ignore_index=True)
 
-    reversed_columns = column[::-1]
-    result_data.columns = reversed_columns + ['label']+['id']
     return result_data
 
 
-data = create_df_data(file_start, dir_list)
-print(data.inf0)
+def spilt_in_window(window_size, overlap=0.1, data=8):
+    for i in range(1, 11):
+        data = 9
+
+
+def select_image_files(subject, task):
+    """
+        Selects specific image files from a ZIP archive based on the subject, task, and frame numbers.
+
+        Parameters:
+        subject (str): Subject identifier.
+        task (str): Task identifier.
+
+        Returns:
+        list: A list of filtered image file paths.
+        """
+    path = r'X:\BP4D+_v0.2\2D+3D'
+    end = subject + '.zip'
+    zip_path = os.path.join(path, end)
+
+    try:
+        with ZipFile(zip_path, 'r') as myzip:
+            # First, filter for only JPG files to reduce the dataset
+            jpg_files = [name for name in myzip.namelist() if name.endswith('.jpg')]
+
+        selected_frames = frames_from_au(subject, task)[:, 0]
+        selected_images = []
+
+        for selected_ in selected_frames:
+            # Substrings to check for each selected frame
+            substrings = [subject, task, f'{selected_}']
+
+            # Filter list based on substrings for each frame
+            filtered_list = [s for s in jpg_files if all(sub in s for sub in substrings)]
+            selected_images.extend(filtered_list)
+        # print(selected_images)
+        return selected_images
+
+    except FileNotFoundError:
+        print(f"File not found: {zip_path}")
+        return []
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return []
+
+
+def frames_from_au(subject, task):
+    """
+    Extracts and merges the second column of Action Unit (AU) data from multiple files,
+    corresponding to a given subject and task. Places the first column (which is the same
+    across all files) as the first column in the final array.
+
+    Parameters:
+    subject (str): The subject identifier.
+    task (str): The task identifier.
+
+    Returns:
+    numpy.ndarray: An array containing the merged AU data.
+    """
+    path = r'X:\BP4D+_v0.2\AUCoding\AU_INT'
+    dir_list = os.listdir(path)
+    name = subject + '_' + task + '_'
+    aus = []
+    column = None
+
+    for dir_ in dir_list:
+        file_name = name + dir_ + '.csv'
+        file_path = os.path.join(path, dir_, file_name)
+
+        try:
+            au_data = np.array(pd.read_csv(file_path, header=None))
+            aus.append(au_data[:, 1])
+            column = au_data[:, 0]
+        except FileNotFoundError:
+            print(f"File not found: {file_path}")
+            continue
+
+    if not aus:
+        return np.array([])
+
+    if column is not None:
+        aus.insert(0, column)
+
+    # Combine all data into a single array
+    AUs = np.array(aus).T
+    return AUs
+
+
+# frames_from_au(subject='F001', task='T1')
+
+select_image_files(subject='F001', task='T1')
--- test.py
+++ test.py
@@ -1,12 +1,72 @@
 import os
 import numpy as np
 import pandas as pd
+import tsfel
+import itertools
+import matplotlib.pyplot as plt
+import matplotlib as mpl
 
-# 假设您有一个字符串列表
-string_list = ["Apple", "Banana", "Cherry", "Date", "Elderberry"]
+name = ['BP Dia_mmHg.txt', 'BP_mmHg.txt', 'EDA_microsiemens.txt', 'LA Mean BP_mmHg.txt', 'LA Systolic BP_mmHg.txt',
+            'Pulse Rate_BPM.txt', 'Resp_Volts.txt', 'Respiration Rate_BPM.txt']
 
-# 使用切片操作反转列表
-reversed_list = string_list[::-1]
 
-# 输出反转后的列表
-print(reversed_list)
+def plot():
+    mpl.use('TkAgg')
+    path = r'X:\BP4D+_v0.2\Physiology\F001\T1'
+
+    fig, axs = plt.subplots(nrows=2, ncols=4, figsize=(16, 8))
+    # Flatten the array of AxesSubplot objects for easier iteration
+    axs = axs.flatten()
+
+    for idx, n in enumerate(name):
+        pa = os.path.join(path, n)
+        signal = pd.read_csv(pa, header=None)
+
+        axs[idx].plot(signal.iloc[:, 0])
+        axs[idx].set_title(f"File: {n}")  # Optional: Set a title for each subplot
+
+    # Hide any unused subplots if there are less than 8 files
+    for idx in range(len(name), len(axs)):
+        fig.delaxes(axs[idx])
+
+    plt.tight_layout()  # Adjust subplots to fit into the figure area
+    plt.show()  # Display the figure
+
+
+def cal():
+    file_start = r"X:\BP4D+_v0.2\Physiology"
+    dir_list = os.listdir(file_start)
+
+    x_train = []
+    labels = []
+    for dir_ in dir_list:
+        for i in range(1, 11):
+            bridge = 'T' + f'{i}'
+            window = []
+            signal_arr = []
+            for name_ in name:
+                new_path = os.path.join(file_start, dir_, bridge, name_)
+                if os.path.exists(new_path):
+                    signal_ = pd.read_csv(new_path, header=None)
+                    signal_arr.append(signal_.values)
+                else:
+                    print(f'{new_path} is not exist')
+            print(dir_, i)
+            signal = np.concatenate(signal_arr, axis=1)
+            w = tsfel.signal_window_splitter(signal=signal, window_size=2000, overlap=0.2)
+            window.append(w)
+            labels.append(np.repeat(i, len(w)))
+        x_train.append(window)
+
+    print(len(x_train), len(x_train[0]), len(x_train[0][0]), len(x_train[0][0][0]))
+
+
+def process(signal):
+    max_size = 0
+    if signal.shape[0] < max_size:
+        # fill
+        padding = np.full((max_size - signal.shape[0], signal.shape[1]), np.nan)
+        signal = np.vstack((signal.values, padding))
+    else:
+        signal = signal.values
+    return signal
